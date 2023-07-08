9fa529f952fe957bccdd74ff4fafb65f036deb58
glasuncion <98256605+glasuncion@users.noreply.github.com>
2023-07-08 16:12:43 -0400

    Add files via upload

--- main.tf
+++ main.tf
@@ -15,13 +15,13 @@ data "aws_vpc" "default" {
 
 # Launch EC2 instance
 resource "aws_instance" "web_instance" {
-  ami                         = data.aws_ami.latest_amazon_linux.id
-  instance_type               = "t2.micro"
-  iam_instance_profile 	      = "LabInstanceProfile"
-  key_name                    = "clo835_assign1"
+  ami                  = "ami-0c94855ba95c71c99"
+  instance_type        = "t2.micro"
+  iam_instance_profile = "LabInstanceProfile"
+  key_name             = "clo835_assign1"
 
- 
- user_data = <<-EOF
+
+  user_data = <<-EOF
     #!/bin/bash
     yum update -y
     amazon-linux-extras install docker -y
